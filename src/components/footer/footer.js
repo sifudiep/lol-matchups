c71b29f111f36a254e16f9d66e8a6c298d338c2c
sifudiep <mikael.diep@gmail.com>
2018-10-18 18:17:31 +0200

    footer checkpoint

---
+++ src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer_contact">
+          <a href="" className="footer_contact_business" />
+          <a href="" className="footer_contact_suggestions" />
+          <a href="" className="footer_contact_support" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
