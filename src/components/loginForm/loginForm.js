8a22bf61eb1f763a7ff7bd78e7b1f9bba1f80733
sifudiep <mikael.diep@gmail.com>
2018-10-29 15:57:17 +0100

    Base login route added

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -4,7 +4,12 @@ class LoginForm extends Component {
   render() {
     return (
       <div className="loginSection_loginForm">
-        <p>LoginForm</p>
+        <input type="text" className="loginSection_loginForm_accountInput" />
+        <input type="password" className="loginSection_loginForm_passInput" />
+        <button className="loginSection_loginForm_loginButton">Login</button>
+        <p className="loginSection_loginForm_signUpText">
+          No account? Sign up!
+        </p>
       </div>
     );
   }
