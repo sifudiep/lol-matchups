9c6e9ffeae5ceb581d110e7798c79ddd184cf3ec
sifudiep <mikael.diep@gmail.com>
2018-11-06 21:42:59 +0100

    k

---
+++ src/backend/services/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  GoogleClientID:
+    "341169993511-tequ0fj2td1mrcj6n92cmr7b5coq67ol.apps.googleusercontent.com",
+  GoogleClientSecret: "jCC6wkjblbcMAdiOA_MwN5aP",
+  JWTPrivateKey:
+    "AV&¤%&/N%&(N&/YIFGF!ajdpofjotETSDGg%G¤&/(¤Jj4t¤/¤TYFffghadDDAaQWexcZF¤!¤&47dsssaf6463Z%S%yu7",
+  CookieSession:
+    "mmn1k2MÖLMÖLJÖJmlasmdöM%M%)/)#jalsd%IUkaskldmalslJ%LJLasldkalsj%LALjASDJ90dj9SAJd0)AJSd09ASjd09AJSf90ASJf09ASJf09Asmg"
+};
