5109343ab317294dd7d2cb2682b9c4fadac29191
sifudiep <mikael.diep@gmail.com>
2018-11-29 10:17:48 +0100

    Fixed ranked calculation, ie rank 20 gets converted to "Diamond V".

--- src/components/user_section/user_section_components/MatchItem.js
+++ src/components/user_section/user_section_components/MatchItem.js
@@ -2,6 +2,44 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class MatchItem extends Component {
+  calculateRank(rankInput) {
+    let division = 5;
+    let tier;
+    for (let i = 0; i < 5; i++) {
+      if (rankInput < 5) {
+        division = rankInput;
+        tier = 0;
+        break;
+      } else if (rankInput % 5 !== 0) {
+        rankInput -= 1;
+        division -= 1;
+      } else {
+        tier = rankInput / 5;
+        break;
+      }
+    }
+
+    switch (tier) {
+      case 0:
+        tier = "Bronze";
+        break;
+      case 1:
+        tier = "Silver";
+        break;
+      case 2:
+        tier = "Gold";
+        break;
+      case 3:
+        tier = "Platinum";
+        break;
+      case 4:
+        tier = "Diamond";
+        break;
+    }
+
+    return `${tier} ${division}`;
+  }
+
   render() {
     return (
       <div className="userSection__matchItem">
@@ -11,7 +49,7 @@ class MatchItem extends Component {
             src={this.props.S1_imgURL}
           />
           <p className="userSection__matchItem__summonerOne__rank">
-            {this.props.S1_rank}
+            {this.calculateRank(this.props.S1_rank)}
           </p>
 
           <p className="userSection__matchItem__summonerOne__lane">
@@ -21,7 +59,7 @@ class MatchItem extends Component {
         <div className="userSection__matchItem__summonerTwo">
           <img
             className="userSection__matchItem__summonerTwo__championIcon"
-            src={this.props.S2_imgURL}
+            src={this.calculateRank(this.props.S2_imgURL)}
           />
           <p className="userSection__matchItem__summonerTwo__rank">
             {this.props.S2_rank}
