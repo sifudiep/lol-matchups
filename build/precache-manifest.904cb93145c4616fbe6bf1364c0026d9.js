bb774288ac8e4848683824de69c4f2ec47d4e0b0
sifudiep <mikael.diep@gmail.com>
2018-12-04 14:59:52 +0100

    build added

---
+++ build/precache-manifest.904cb93145c4616fbe6bf1364c0026d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c6da6f0663be20259a1",
+    "url": "/static/js/main.0c6da6f0.chunk.js"
+  },
+  {
+    "revision": "96bfc93575681dff9d20",
+    "url": "/static/js/1.96bfc935.chunk.js"
+  },
+  {
+    "revision": "0c6da6f0663be20259a1",
+    "url": "/static/css/main.84151640.chunk.css"
+  },
+  {
+    "revision": "254acb6bc28ab70d5035639f29f2144e",
+    "url": "/index.html"
+  }
+];
